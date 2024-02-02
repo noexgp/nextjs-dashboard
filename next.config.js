228e8629a8c94879eea23a6d3c9bb942375ed26d
Glenn Palic <noexgp@gmail.com>
2024-02-02 20:44:57 +0800

    remove logo
    remove loog

--- next.config.js
+++ next.config.js
@@ -3,10 +3,22 @@ const nextConfig = {};
 
 module.exports = nextConfig;*/
 
+//module.exports = {
+// eslint: {
+// Warning: This allows production builds to successfully complete even if
+// your project has ESLint errors.
+//ignoreDuringBuilds: true,
+// },
+//};
+
 module.exports = {
+  webpack: (config, { dev, isServer }) => {
+    // Disable caching for both client and server
+    config.cache = true;
+
+    return config;
+  },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
